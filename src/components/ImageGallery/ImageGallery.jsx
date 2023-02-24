b3e120c2c70a745ef490afb4a6d88846ed715851
tsylepa <tsylepa@gmail.com>
2023-02-24 13:38:35 +0000

    add alt to img

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,10 +2,14 @@ import { Gallery, Item } from './ImageGallery.styled';
 
 const ImageGallery = ({ data, openModal }) => (
   <Gallery>
-    {data.map(({ id, webformatURL, largeImageURL }) => {
+    {data.map(({ id, webformatURL, largeImageURL, tags }) => {
       return (
         <Item key={id}>
-          <img src={webformatURL} onClick={() => openModal(largeImageURL)} />
+          <img
+            src={webformatURL}
+            onClick={() => openModal(largeImageURL)}
+            alt={tags}
+          />
         </Item>
       );
     })}
